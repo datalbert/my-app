3a92f120e0feb6e69cb3e086d1f3800402b7b644
datalbert <albertoavfdez2000@gmail.com>
2023-02-17 10:30:57 +0100

    Nombre del proyecto:my-ap, Descripcion del proyecto de prueba

---
+++ src/main/java/com/mycompany/myapp/entity/TestEntity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.mycompany.myapp.entity;
+
+/**
+ * @author albertoavilafernandez
+ *
+ */
+public class TestEntity {
+	private String nombre;
+
+	/*propiedad de prueba para la muestra de compilacion del archivo testentity*/
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
